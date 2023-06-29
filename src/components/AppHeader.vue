7cb3770ffaa2bca8cf64ecd163ac10c56a7a2960
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-29 16:25:25 +0200

    nav header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -65,10 +65,39 @@ export default {
     <nav>
       <img src="../assets/img/dc-logo.png" alt="logo">
       <ul>
-        <li v-for:"component in navComponents"><a href="{{ url }}">{{ text }}</a></li>
+        <li v-for="(component, i) in navComponents" :key="i" :class="{ active: component.current }"><a
+            href="{{ component.url }}">{{
+              component.text }}</a></li>
       </ul>
     </nav>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.active {
+  border-bottom: 7px solid rgb(2, 130, 249);
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+
+  img {
+    width: 100px;
+  }
+
+  ul {
+    display: flex;
+    flex-basis: 70%;
+    justify-content: flex-end;
+
+    li {
+      margin: 0 8px;
+      align-items: center;
+      height: 100px;
+      display: flex;
+    }
+  }
+}
+</style>
